5e93f2df01d66c3e7c86746d09e29d75a44ed77c
Daniel Danner <daniel@danner.de>
2025-08-11 19:08:09 +0200

    Incorporate neverpanic's notifications and cleanup

--- restic-android.sh
+++ restic-android.sh
@@ -16,6 +16,9 @@ RESTIC_ENV_FILE="$HOME/.config/restic-android.env"
 SOURCE_DIR="/storage/emulated/0"
 RESTIC_CACHE_DIR="$PREFIX/var/cache/restic"
 
+declare -a TERMUX_NOTIFICATIONS=()
+TERMUX_NOTIFICATION_ID="restic-${HOSTNAME}"
+
 ensure_log_and_reexec() {
     mkdir -p "$LOG_DIR"
     if [ "${RESTIC_BACKUP_LOGGING:-}" != "1" ]; then
@@ -70,34 +73,109 @@ ensure_schedule() {
     fi
 }
 
+notify() {
+    local persist=$1
+    shift
+    local id="$1"
+    shift
+    local -a args=("--group" "${TERMUX_NOTIFICATION_ID}" "--id" "$id")
+
+    if termux-notification "${args[@]}" "$@"; then
+        if [ "$persist" -eq 0 ]; then
+            TERMUX_NOTIFICATIONS+=("$id")
+        fi
+    fi
+}
+
+msg() {
+    echo "***" "$@"
+}
+
+info() {
+    msg "INFO:" "$@"
+    termux-toast -s "$*"
+}
+
+warn() {
+    msg "WARN:" "$@"
+    echo "Warning:" "$@" | \
+        notify 1 failure \
+        --title "restic" \
+        --alert-once \
+        --priority low
+}
+
+err() {
+    msg "ERROR:" "$@"
+    echo "Error:" "$@" | \
+        notify 1 failure \
+        --title "restic" \
+        --alert-once \
+        --priority high
+    exit 1
+}
+
+cleanup() {
+  for notification in "${TERMUX_NOTIFICATIONS[@]}"; do
+    termux-notification-remove "$notification"
+  done
+  termux-wake-unlock
+}
+
+register_exit_cleanup() {
+    trap "cleanup" EXIT
+    termux-wake-lock
+    notify 0 progress \
+        --alert-once \
+        --ongoing \
+        --priority low \
+        --title "restic" \
+        --content "Running backup for ${HOSTNAME}"
+}
+
+
 main() {
     ensure_log_and_reexec "$@"
     ensure_schedule
 
     if [ ! -r "$RESTIC_ENV_FILE" ]; then
-        echo "Restic environment file not found: $RESTIC_ENV_FILE" >&2
-        exit 1
+        err "Restic environment file not found: $RESTIC_ENV_FILE"
     fi
     set -a  # Marks modified/created variables for export
     . "$RESTIC_ENV_FILE"
 
     if ! is_wifi_connected; then
-        echo "Not connected to Wi-Fi. Exiting." >&2
+        msg "Not connected to Wi-Fi. Exiting."
         exit 1
     fi
 
     if ! is_charging; then
-        echo "Device not charging. Exiting." >&2
+        msg "Device not charging. Exiting."
         exit 1
     fi
 
     mkdir -p "$RESTIC_CACHE_DIR"
     export RESTIC_CACHE_DIR
     restic unlock
+
+    register_exit_cleanup
+
+    termux-wake-lock
+    notify 0 progress \
+      --alert-once \
+      --ongoing \
+      --priority low \
+      --title "restic" \
+      --content "Running backup"
+
     restic backup "$SOURCE_DIR" &
     local restic_pid=$!
     monitor_conditions "$restic_pid"
     wait "$restic_pid"
+    if [ $? -ne 0 ]; then
+        err "Backup failed!"
+    fi
+    info "Backup finished successfully"
 }
 
 main "$@"
